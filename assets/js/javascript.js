f6d2d3d1a430641d3db929e04080bbd84b0ce82e
Javeriya Saeed Abbasi <jiyasaeed333@gmail.com>
2022-04-01 18:25:24 +0500

    min-cards

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,34 +1,48 @@
 // <!-- Initialize Swiper -->
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    loop:true,
-    autoplay:true,
-    autoplaySpeed: 1500,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+var swiper = new Swiper(".mySwiper",  {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-        414: {
-          slidesPerView: 1,
-           spaceBetween: 10
-        },
-        360: {
-        slidesPerView: 1,
-        spaceBetween: 10
-        }
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-  });
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    414: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    360: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    }
+  },
+});
+
+
+
+// ==================== WOW JS ============================= //
+new WOW().init();
+// ==================== Butter JS ========================== //
+butter.init({
+  wrapperId: 'butter'
+});
