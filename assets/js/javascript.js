d4a08fcce2f12c6e50a2b951fc1e131f46b1a46b
Javeriya Saeed Abbasi <jiyasaeed333@gmail.com>
2022-04-04 16:58:12 +0500

    animations left

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,5 @@
 // <!-- Initialize Swiper -->
-var swiper = new Swiper(".mySwiper",  {
+var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 10,
   loop: true,
@@ -45,4 +45,35 @@ new WOW().init();
 // ==================== Butter JS ========================== //
 butter.init({
   wrapperId: 'butter'
+});
+// =================== MOBILE SCREEN SIDE NAV =============== //
+var sidebarBox = document.querySelector('#box');
+var sidebarBtn = document.querySelector('#btn');
+var pageWrapper = document.querySelector('#main-content');
+
+sidebarBtn.addEventListener('click', function (event) {
+
+  if (this.classList.contains('active')) {
+    this.classList.remove('active');
+    sidebarBox.classList.remove('active');
+  } else {
+    this.classList.add('active');
+    sidebarBox.classList.add('active');
+  }
+});
+
+pageWrapper.addEventListener('click', function (event) {
+
+  if (sidebarBox.classList.contains('active')) {
+    sidebarBtn.classList.remove('active');
+    sidebarBox.classList.remove('active');
+  }
+});
+
+window.addEventListener('keydown', function (event) {
+
+  if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
+    sidebarBtn.classList.remove('active');
+    sidebarBox.classList.remove('active');
+  }
 });
